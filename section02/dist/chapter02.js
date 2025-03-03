2b137e3ed6f3291fedc83063ebe4dce45555307c
kimHyeonJu <kimhyesom1202@gmail.com>
2025-03-03 17:26:07 +0900

    ✨ 2. 타입스크립트 기본 - 배열 - 튜플

---
+++ section02/dist/chapter02.js
@@ -0,0 +1,15 @@
+// 배열
+// 예) numArr 변수를 만들고 이 변수의 초기값으로 3개의 숫자를 담는 배열을 만듦
+// 1. 숫자를 담는 배열
+let numArr = [1, 2, 3];
+// 2. 문자열을 담는 배열
+let strArr = ["a", "b", "c"];
+// 3. 불리언값을 담는 배열
+// let boolArr: boolean[] = [true, false];
+// 3-1. Generic 문법 : TS에서 Array< 다른타입 >
+let boolArr = [true, false];
+// 예) 배열에 들어가는 요소들의 타입이 다양한 경우
+// Tip) TS에서 어떻게 정의를 해야할 지 잘 모르겠을 때 → 마우스를 변수 위에 올려보자
+// 4. 
+let multiArr = [1, 'hello'];
+export {};
